dd61ada56389be14d9f943a644279c9672de9294
Pedro Madureira <dev@phsolucoesweb.com.br>
2022-01-01 16:17:54 -0300

    commentaries removed

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -5,5 +5,4 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     def ready(self):
-        print('?????????????????????//')
         import core.signals
--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -226,35 +226,35 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 ##  storage configuration in S3 AWS
 
-#  AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-#  if AWS_ACCESS_KEY_ID:
-    #  STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    #  INSTALLED_APPS.append('s3_folder_storage')
-    #  INSTALLED_APPS.append('storages')
-    #  AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    #  AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    #  AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
-    #  AWS_PRELOAD_METADATA = True
-    #  AWS_AUTO_CREATE_BUCKET = False
-    #  AWS_QUERYSTRING_AUTH = True
-    #  AWS_S3_CUSTOM_DOMAIN = None
-    #  AWS_DEFAULT_ACL = 'private'
+if AWS_ACCESS_KEY_ID:
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    INSTALLED_APPS.append('s3_folder_storage')
+    INSTALLED_APPS.append('storages')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+    AWS_PRELOAD_METADATA = True
+    AWS_AUTO_CREATE_BUCKET = False
+    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_CUSTOM_DOMAIN = None
+    AWS_DEFAULT_ACL = 'private'
 
-##    ---/Upload Media Folder
+#    ---/Upload Media Folder
 
-    #  DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
-    #  DEFAULT_S3_PATH = 'media'
-    #  MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
-    #  MEDIA_URL = f'//{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{DEFAULT_S3_PATH}/'
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    DEFAULT_S3_PATH = 'media'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{DEFAULT_S3_PATH}/'
 
- ##   -----/Static assets
+ #   -----/Static assets
 
-    #  STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
-    #  STATIC_S3_PATH = 'static'
-    #  STATIC_ROOT = f'/{STATIC_S3_PATH}/'
-    #  STATIC_URL = f'//{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{STATIC_S3_PATH}/'
-    #  ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_S3_PATH = 'static'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # axex
 AXES_FAILURE_LIMIT = 5
